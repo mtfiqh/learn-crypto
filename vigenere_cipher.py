6887ea4d868d45094628a4fee2bbdd815fb7016d
Muhammad Taufiq Hidayat <mtfiqh@gmail.com>
2019-09-27 22:35:58 +0700

    vigenere_cipher done

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -18,6 +18,7 @@ def charKeyToDecimal(charKey):
     return decimalKey
 
 def encrypt(plainText, key):
+    key = key.replace(' ','')
     idxText = 0
     encryptText = ""
     idxKey  = 0
@@ -45,6 +46,7 @@ def encrypt(plainText, key):
     return encryptText
 
 def decrypt(encryptText, key):
+    key = key.replace(' ','')
     idxEncrypt = 0
     decryptText = ""
     idxKey = 0
@@ -70,48 +72,87 @@ def decrypt(encryptText, key):
     return decryptText
 
 # ---------------------------------- END CORE ---------------------------
+def saveFile(nama, isi):
+    # save file
+    file = open(nama+'.txt', "w+")
+    file.write(isi)
+    file.close()
+    print('saved in ',nama,'.txt"')
 
-while (1):
+def encryptFromFile():
+    try:
+        file_name = input("Your file name, include extension (ex: file.txt): ")
+        file = open(file_name, 'r')
+        plainText = file.read()
+        file.close()
+        if plainText:
+            print('Loaded')
+        else:
+            print('The file is empty!')
+            print('')
+            return None
+    except Exception as e:
+        print('error: ', e)
+        print('')
+        return None
+    key = input('Key  : ')
+    encrypted = encrypt(plainText, key)
+    print('Text :', plainText)
+    print('Encrypted    :', encrypted)
+    saveFile('Vigenere_encrypted', encrypted)
+
+def encryptFromText():
+    text = input('Text  : ')
+    key = input('Key : ')
+    encrypted = encrypt(text, key)
+    print('Encrypted    :', encrypted)
+    saveFile('Vigenere_encrypted', encrypted)
+
+def decryptFromFile():
+    try:
+        file_name = input("Your file name, include extension (ex: file.txt): ")
+        file = open(file_name, 'r')
+        plainText = file.read()
+        file.close()
+        if plainText:
+            print('Loaded')
+        else:
+            print('The file is empty!')
+            print('')
+            return None
+    except Exception as e:
+        print('error: ', e)
+        print('')
+        return None
+    key = input('Key  : ')
+    decrypted = decrypt(plainText, key)
+    print('Text :', plainText)
+    print('Decrypted    :', decrypted)
+    saveFile('Vigenere_decrypted', decrypted)
+
+def decryptFromText():
+    text = input('Text  : ')
+    key = input('Key : ')
+    decrypted = decrypt(text, key)
+    print('Decrypted    :', decrypted)
+    saveFile('Vigenere_decrypted', decrypted)
+
+while(1):
+    print('Vigenere Cipher!! -- github.com/mtfiqh/learn-crypto --')
     print('1.   Load text from file [Encrypt]')
     print('2.   Input text from keyboard [Encrypt]')
+    print('3.   Input text from file [Decrypt]')
+    print('4.   Input text from keyboard [Decrypt]')
+    print('5.   Exit')
     choice = input('Choice: ')
     if choice == '1':
-        try:
-            file_name = input("Your file name, include extension (ex: file.txt): ")
-            file = open(file_name, 'r')
-            plainText = file.read()
-            file.close()
-            if plainText:
-                break
-            else:
-                print('The file is empty!')
-                print('')
-        except Exception as e:
-            print('error: ', e)
-            print('')
-
+        encryptFromFile()
     elif choice == '2':
-        plainText = input("Enter text to encrypt: ")
-        break
+        encryptFromText()
+    elif choice == '3':
+        decryptFromFile()
+    elif choice == '4':
+        decryptFromText()
     else:
         print('Your choice is wrong !')
-    
-
-key = input("Enter the key: ")
-# removing spaces in key
-key = key.replace(' ','')
-print('')
-print('')
-print('Plain Text   = ', plainText)
-print('Key          =', key)
-print('=============================')
-encrypted = (encrypt(plainText, key))
-print('Encrypt Text = ', encrypted)
-decrypted = decrypt(encrypted, key)
-print('Decrypt Text = ',decrypted)
-
-# save file
-file = open('Vigenere Encrypt Text.txt', "w+")
-file.write(encrypted)
-file.close()
-print('Encrypted text saved in "Vigenere Encrypt Text.txt"')
+    print('\n\n')
